043596234da26c75ccf0450a89cfc38b87f9d0d8
Anna <65027003+anfnn@users.noreply.github.com>
2024-06-01 16:02:34 +0500

    Delete 1_LABA directory

--- 1_LABA/3.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-void main(int argum, char *armas[]){
-        printf("Kolichestvo argumentov: %d\n", argum);
-        for ( int i = 0;i< argum ;i++){
-            printf("Argument %d: %s\n", i ,armas[i]);
-            }
-    }
